1f75e15ba3b12caeb36d5b242ca0a87641203f4d
JAYJAYTEE12 <awesomeque12@gmail.com>
2020-09-04 15:06:58 +0100

    Added support for tables
    various bug fixes.
    Added -
      - Help Command
      - Credits Command

--- reego.org/scripts/script.js
+++ reego.org/scripts/script.js
@@ -11,7 +11,24 @@ $( document ).ready(function() {
         var inputVal = document.getElementById("line").value;
         if(inputVal == ""){
             return;
-        }
+		}
+		resetDisplay();
+		$("#commandid").children().css('margin-top', "50px");
+		if(inputVal == "help"){
+			$("#helpid").css('display', "block");
+		}else if(inputVal == "credits"){
+			$("#creditsid").css('display', "block");
+		}else{
+			$('#line').val('');
+			$("#commandid").children().css('margin-top', "0px");
+			return;
+		}
+		
         $('#line').val('');
-    }
+	}
+	
+	function resetDisplay(){
+		$("#helpid").css('display', "none");
+		$("#creditsid").css('display', "none");
+	}
 });
