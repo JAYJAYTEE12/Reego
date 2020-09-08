2a8e45ac53b1ad1e4211024c8af2b54d1a514d95
JAYJAYTEE12's Laptop <awesomeque12@gmail.com>
2020-09-08 21:23:18 +0100

    Added github/youtube to commands.
    various bug fixes.

--- reego.org/scripts/script.js
+++ reego.org/scripts/script.js
@@ -18,10 +18,14 @@ $( document ).ready(function() {
 			$("#helpid").css('display', "block");
 		}else if(inputVal == "credits"){
 			$("#creditsid").css('display', "block");
-		}else{
-			$('#line').val('');
+		}else if(inputVal == "github"){
+			window.open('https://github.com/JAYJAYTEE12', "_self");
 			$("#commandid").children().css('margin-top', "0px");
-			return;
+		}else if(inputVal == "content"){
+			window.open('https://www.youtube.com/channel/UCO01SOwAk8225gyBHPya1ig', "_self");
+			$("#commandid").children().css('margin-top', "0px");
+		}else{
+			$("#errorid").css('display', "block");
 		}
 		
         $('#line').val('');
@@ -30,5 +34,6 @@ $( document ).ready(function() {
 	function resetDisplay(){
 		$("#helpid").css('display', "none");
 		$("#creditsid").css('display', "none");
+		$("#errorid").css('display', "none");
 	}
 });
